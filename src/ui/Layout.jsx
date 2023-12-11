2001db25d595b5b6920164cd0d1c8446b1b013e5
JeanCornelio <jcornelo@isf.do>
2023-12-11 14:46:21 -0400

    menu mobile

--- src/ui/Layout.jsx
+++ src/ui/Layout.jsx
@@ -1,20 +1,20 @@
-import { Outlet } from "react-router-dom"
-import { HeaderPage } from "../ui"
-import { Main } from "./Main"
-import { Navbar } from "./navbar"
+import { Outlet } from 'react-router-dom'
+import { HeaderPage } from '../ui'
+import { Main } from './Main'
+import { Navbar } from './navbar'
 
 export const Layout = () => {
   return (
-    <div className="flex h-screen ">
-          <Navbar/>  
-          
-        <div className="relative w-full">
-          <HeaderPage/>
-          <Main>
-            <Outlet/>
+    <div className='flex h-screen relative '>
+      <Navbar />
+
+      <div className='relative w-full'>
+        <HeaderPage />
+        <Main>
+          <Outlet />
         </Main>
-        </div>
-        
+      </div>
+
     </div>
   )
 }
