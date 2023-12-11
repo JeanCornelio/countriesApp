4d0b5f197d99d9c9eeac1c2ff566d2a4361f061c
JeanCornelio <jcornelo@isf.do>
2023-12-11 11:03:04 -0400

    finish

--- src/route/CountriesRoutes.jsx
+++ src/route/CountriesRoutes.jsx
@@ -1,24 +1,21 @@
-import { Navigate, Route, Routes } from "react-router-dom"
-import { CapitalPage, RegionPage, CountriesPage, CountriePage, HomePage } from "../contries"
-import { Layout } from "../ui"
-import { Page404 } from "../404/Page404"
-
-
+import { Navigate, Route, Routes } from 'react-router-dom'
+import { CapitalPage, RegionPage, CountriesPage, CountriePage, HomePage } from '../contries'
+import { Layout } from '../ui'
+import { Page404 } from '../404/Page404'
 
 export const CountriesRoutes = () => {
-  return (  
-   <Routes>
-         <Route path="/home" element={<HomePage/>}></Route>
-         <Route path="/404" element={<Page404/>}></Route>
-         <Route path="/" element={<Layout/>}>
-            <Route path="/countries"  element={<CountriesPage/>}></Route>
-            <Route path="/region"  element={<RegionPage/>}></Route>
-            <Route path="/capital"  element={<CapitalPage/>}></Route>
-            <Route path="/countrie/:countrieCode"  element={<CountriePage/>}></Route>
-         </Route>
-         <Route path="/*"  element={<Navigate to='/404'/>}></Route> 
-   </Routes>
-  
- 
+  return (
+    <Routes>
+      <Route path='/' element={<HomePage />} />
+      <Route path='/404' element={<Page404 />} />
+      <Route path='/' element={<Layout />}>
+        <Route path='/countries' element={<CountriesPage />} />
+        <Route path='/region' element={<RegionPage />} />
+        <Route path='/capital' element={<CapitalPage />} />
+        <Route path='/countrie/:countrieCode' element={<CountriePage />} />
+      </Route>
+      <Route path='/*' element={<Navigate to='/404' />} />
+    </Routes>
+
   )
 }
